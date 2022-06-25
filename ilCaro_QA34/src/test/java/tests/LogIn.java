fa440d6e3e5c6bb9e1525b710d21ff27dda5be33
DianaGru <dianchik2004@gmail.com>
2022-06-25 16:26:28 +0300

    Test LogIn

---
+++ ilCaro_QA34/src/test/java/tests/LogIn.java
@@ -0,0 +1,83 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class LogIn extends TestBase{
+
+
+    @Test
+    public void logInSuccess(){
+
+       openLoginForm();
+       fillLoginForm("emmabr@gmail.com", "E12345n$");
+       submitLogin();
+       logOut();
+
+    }
+
+    @Test
+    public void logInUnsuccessful1(){
+        openLoginForm();
+        fillLoginForm("emmabrgmail.com", "E12345n$");
+        submitLogin();
+        logOut();
+
+    }
+
+    @Test
+    public void logInUnsuccessful2(){
+        openLoginForm();
+        fillLoginForm("emmabr@gmailcom", "E12345n$");
+        submitLogin();
+        logOut();
+
+    }
+
+    @Test
+    public void logInUnsuccessful3(){
+        openLoginForm();
+        fillLoginForm("emmabr@gmail.co", "E12345n$");
+        submitLogin();
+        logOut();
+
+    }
+
+    @Test
+    public void logInUnsuccessful4(){
+        openLoginForm();
+        fillLoginForm("emmabr@gmail.com", "E12345n");
+        submitLogin();
+        logOut();
+
+    }
+
+    @Test
+    public void logInUnsuccessful5(){
+        openLoginForm();
+        fillLoginForm("emmabr@gmail.com", "e12345n$");
+        submitLogin();
+        logOut();
+
+    }
+
+    @Test
+    public void logInUnsuccessful6(){
+        openLoginForm();
+        fillLoginForm("emmabr@gmail.com", "E12345N$");
+        submitLogin();
+        logOut();
+
+    }
+
+    @Test
+    public void logInUnsuccessful7(){
+        openLoginForm();
+        fillLoginForm("emmabr@gmail.com", "E54321n$");
+        submitLogin();
+        logOut();
+
+    }
+
+}
---
+++ ilCaro_QA34/src/test/java/tests/TestBase.java
@@ -0,0 +1,60 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class TestBase {
+    WebDriver wd;
+
+    @BeforeMethod
+    public void preCondition() {
+
+        wd = new ChromeDriver();
+        wd.navigate().to("https://ilcarro-1578153671498.web.app/");
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    public void openLoginForm() {
+
+       WebElement logInTab = wd.findElement(By.xpath("//*[contains(@href, '/login')]"));
+        logInTab.click();
+    }
+
+    public void fillLoginForm(String email, String password) {
+        type(By.cssSelector("#email"), email);
+        type(By.cssSelector("#password"), password);
+    }
+
+    public void type(By locator, String text) {
+        if (text != null) {
+            WebElement element = wd.findElement(locator);
+            element.click();
+            element.clear();
+            element.sendKeys(text);
+        }
+
+    }
+
+    public void submitLogin() {
+        wd.findElement(By.cssSelector("button[type='submit']")).click();
+        wd.findElement(By.cssSelector(".positive-button")).click();
+    }
+
+    public void logOut(){
+        WebElement logOutButton = wd.findElement(By.xpath("//*[contains(@href, '/logout')]"));
+        logOutButton.click();
+    }
+
+    @AfterMethod
+    public void exit(){
+        wd.quit();
+    }
+}
